e047f9310f44eb07c8385d1e2b6f1401e3e4d4d5
Mohcka <mikeduels@yahoo.com>
2020-07-04 18:39:01 -0700

    Adds collection 📚

--- Models/RedditCloneDatabaseSettings.cs
+++ Models/RedditCloneDatabaseSettings.cs
@@ -6,6 +6,8 @@ namespace reddit_clone_api.Models
 {
   public class RedditCloneDatabaseSettings : IRedditCloneDatabaseSettings
   {
+    public string secret { get; set; }
+    public string UsersCollectionName { get; set; }
     public string PostsCollectionName { get; set; }
     public string ConnectionString { get; set; }
     public string DatabaseName { get; set; }
@@ -13,6 +15,8 @@ namespace reddit_clone_api.Models
 
   public interface IRedditCloneDatabaseSettings
   {
+    string secret { get; set; }
+    string UsersCollectionName { get; set; }
     string PostsCollectionName { get; set; }
     string ConnectionString { get; set; }
     string DatabaseName { get; set; }
