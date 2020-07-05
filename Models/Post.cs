47d2d60edfd6f1a8c4b159fc5b2a8c9f90e68d0d
Mohcka <mikeduels@yahoo.com>
2020-07-04 18:39:27 -0700

    Adds required constraint 📚

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,18 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace reddit_clone_api.Models
 {
-    public class Post
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+  /// <summary>
+  /// TODO: Write docs
+  /// </summary>
+  public class Post
+  {
+    [BsonId]
+    [Required]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
 
-        [BsonElement("Title")]
-        public string PostTitle { get; set; }
-        [BsonElement("Post")]
-        public string PostContent { get; set; }
+    [BsonElement("Title")]
+    [Required]
+    public string PostTitle { get; set; }
+    [BsonElement("Post")]
+    [Required]
+    public string PostContent { get; set; }
 
-    }
+    [Required]
+    public string UserId { get; set; }
+
+  }
 }
