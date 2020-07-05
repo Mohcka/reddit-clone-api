e46bb9bab430cb1b48d0cb8d9002f25d599dcd2f
Mohcka <mikeduels@yahoo.com>
2020-07-04 18:42:53 -0700

    Adds user model 🖼

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace reddit_clone_api.Models
+{
+  /// <summary>
+  /// TODO: Write docs
+  /// </summary>
+  public class User
+  {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    [Required]
+    public string Id { get; set; }
+    [BsonElement("username")]
+    [Required]
+    public string UserName { get; set; }
+
+    [JsonIgnore] // For security, ignore this when serialzing
+    [BsonElement("password")]
+    [Required]
+    public string Password { get; set; }
+  }
+}
