f733b5e24367c54d5b8cf0a3443a8c7f0e0254fa
Mohcka <mikeduels@yahoo.com>
2020-07-13 10:42:23 -0700

    Adds vote methods 🛎

--- Services/VoteService.cs
+++ Services/VoteService.cs
@@ -10,6 +10,8 @@ namespace reddit_clone_api.Services
   public interface IVoteService : IBaseServiceMDB<Vote>
   {
     Task<List<Vote>> GetUserVotes(string userId);
+    Task<Vote> FindByUserAndResponseId(string userId, string responseId);
+    
   }
 
   public class VoteService : BaseServiceMDB<Vote>, IVoteService
@@ -21,6 +23,14 @@ namespace reddit_clone_api.Services
 
     public async Task<List<Vote>> GetUserVotes(string userId) {
       return await _db.Find<Vote>(v => v.UserId == userId).ToListAsync();
+    }
+
+    public async Task<Vote> FindByUserId(string userId) {
+      return await _db.Find<Vote>(v => v.UserId == userId).FirstOrDefaultAsync();
+    }
+
+    public async Task<Vote> FindByUserAndResponseId(string userId, string responseId) {
+      return await _db.Find<Vote>(v => v.UserId == userId && v.ResponseId == responseId).FirstOrDefaultAsync();
     }
   }
 }
